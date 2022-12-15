a97bf549e58108d6ced67686c95f0e9229b4c8d6
Naomie SABBAH <naomie.sab@gmail.com>
2022-12-15 16:22:14 +0100

    pagelevel queries

--- main.py
+++ main.py
@@ -30,7 +30,18 @@ if __name__ == "__main__":
    write_gsheet(GSHEET_CONN, result_db, 'nbrofsessionspervisitor')
    print(f'Complete, go to https://docs.google.com/spreadsheets/d/{sheet_id}/edit?usp=sharing')
 
-# Query 3 connect to the gsheet - nextsessionreturnrate
+# Query 3 connect to the gsheet - nbr of sessions before converting
+
+if __name__ == "__main__":
+   result_db = get_query_results(QUERY=query_nbrofsessionsbeforeconverting, project_id=project_id,
+                                 page_condition=page_condition,
+                                 start_date=start_date, end_date=end_date,
+                                 device_id=device_id)
+   GSHEET_CONN = connect_gsheet(sheet_id=sheet_id)
+   write_gsheet(GSHEET_CONN, result_db, 'nbrofsessionsbeforeconverting')
+   print(f'Complete, go to https://docs.google.com/spreadsheets/d/{sheet_id}/edit?usp=sharing')
+
+# Query 4 connect to the gsheet - nextsessionreturnrate
 if __name__ == "__main__":
    result_db = get_query_results(QUERY=query_nextsessionreturnrate, project_id=project_id,
                                  page_condition=page_condition,
@@ -40,7 +51,7 @@ if __name__ == "__main__":
    write_gsheet(GSHEET_CONN, result_db, 'nextsessionreturnrate')
    print(f'daydiffebetweentwosessions completed')
 
-# Query 4 connect to the gsheet - daydiffebetweentwosessions
+# Query 5 connect to the gsheet - daydiffebetweentwosessions
 if __name__ == "__main__":
    result_db = get_query_results(QUERY=query_daydiffebetweentwosessions, project_id=project_id,
                                  page_condition=page_condition,
@@ -51,7 +62,7 @@ if __name__ == "__main__":
    print(f'daydiffebetweentwosessions completed')
 
 
-# Query 5 connect to the gsheet - hourdiffebetweentwosessions
+# Query 6 connect to the gsheet - hourdiffebetweentwosessions
 if __name__ == "__main__":
    result_db = get_query_results(QUERY=query_hourdiffebetweentwosessions, project_id=project_id,
                                  page_condition=page_condition,
@@ -61,7 +72,7 @@ if __name__ == "__main__":
    write_gsheet(GSHEET_CONN, result_db, 'hourdiffebetweentwosessions')
    print(f'hourdiffebetweentwosessions completed')
 
-# Query 6 connect to the gsheet - boucereturners
+# Query 7 connect to the gsheet - boucereturners
 if __name__ == "__main__":
    result_db = get_query_results(QUERY=query_boucereturners, project_id=project_id,
                                  page_condition=page_condition,
@@ -72,14 +83,39 @@ if __name__ == "__main__":
    print(f'boucereturners completed')
 
 
-# Page level
+#Page level
 
-# # Query 7 connect to the gsheet - boucereturners
-# if __name__ == "__main__":
-#    result_db = get_query_results(QUERY=query_nbrofsessionspeonthepage, project_id=project_id,
-#                                  page_condition=page_condition,
-#                                  start_date=start_date, end_date=end_date,
-#                                  device_id=device_id)
-#    GSHEET_CONN = connect_gsheet(sheet_id=sheet_id)
-#    write_gsheet(GSHEET_CONN, result_db, 'nbrofsessionspeonthepage')
-#    print(f'nbrofsessionspeonthepage completed')
+# Query 8 connect to the gsheet - nbr of sessions per visitors page level
+if __name__ == "__main__":
+   result_db = get_query_results(QUERY=query_nbrofsessionspervisitorpagelevel, project_id=project_id,
+                                 page_condition=page_condition,
+                                 start_date=start_date, end_date=end_date,
+                                 device_id=device_id)
+   GSHEET_CONN = connect_gsheet(sheet_id=sheet_id)
+   write_gsheet(GSHEET_CONN, result_db, 'nbrofsessionspervisitorpagelevel')
+   print(f'nbrofsessionspervisitorpagelevel completed')
+
+
+# Query 9 connect to the gsheet - returonthesiteafterexitingonapage
+if __name__ == "__main__":
+   result_db = get_query_results(QUERY=query_returonthesiteafterexitingonapage, project_id=project_id,
+                                 page_condition=page_condition,
+                                 start_date=start_date, end_date=end_date,
+                                 device_id=device_id)
+   GSHEET_CONN = connect_gsheet(sheet_id=sheet_id)
+   write_gsheet(GSHEET_CONN, result_db, 'returonthesiteafterexitingonapage')
+   print(f'returonthesiteafterexitingonapage completed')
+
+# Query 10 connect to the gsheet - nbr of sessions per visitors page level
+if __name__ == "__main__":
+   result_db = get_query_results(QUERY=query_boucerspagelevelcomingbackonthesite, project_id=project_id,
+                                 page_condition=page_condition,
+                                 start_date=start_date, end_date=end_date,
+                                 device_id=device_id)
+   GSHEET_CONN = connect_gsheet(sheet_id=sheet_id)
+   write_gsheet(GSHEET_CONN, result_db, 'boucerspagelevelcomingbackonthesite')
+   print(f'boucerspagelevelcomingbackonthesite completed')
+
+
+
+
--- queries.py
+++ queries.py
@@ -17,7 +17,7 @@ SELECT device_id, nbrofsessions, uniqExact(user_id) as nbrofusers
     FROM
     (
 SELECT  uniqExact(session_number) as sum_session, user_id ,
-       if(sum_session >10, 10, sum_session) as nbrofsessions, device_id
+       if(sum_session >5, 5, sum_session) as nbrofsessions, device_id
 
 
 
@@ -34,18 +34,48 @@ ORDER BY nbrofsessions asc
 SETTINGS distributed_group_by_no_merge = 0;
   """
 
+query_nbrofsessionsbeforeconverting = """ 
+select  device_id, avg(first_transaction_session) as avg_sessions_before_first_transaction
+    from (
+                     select user_id, session_number
+             from sessions
+WHERE project_id = {project_id}
+AND device_id in ({device_id})
+AND session_date >= '2021-02-01'
+
+AND session_number = 1
+
+             ) as t1
+         left join
+         (  select project_id,
+                    device_id,
+                    user_id,
+                    min(session_number) as first_transaction_session
+             from sessions
+WHERE project_id = {project_id}
+AND device_id in ({device_id})
+AND session_date >= '2021-02-01'
+
+               and session_number_of_transactions > 0
+             group by project_id, device_id, user_id
+
+
+             ) as t2 on t1.user_id = t2.user_id
+             where not project_id = 0 and not device_id = 0
+             group by device_id
+             order by  device_id
+          settings distributed_group_by_no_merge = 0; """
 
 query_nextsessionreturnrate = """ 
 SELECT
 
-    uniqExact(user_id_filter.session_id)          as exit_firstsession_onthewishlist,
-    uniqExact(next_session_info.session_id)       as return_secondsession_wishlist,
-    return_secondsession_wishlist/ exit_firstsession_onthewishlist as sessionsreturnrate
-
+    device_id, uniqExact(user_id_filter.session_id)          as exit_firstsession,
+    uniqExact(next_session_info.session_id)       as return_secondsession,
+    return_secondsession/ exit_firstsession as sessionsreturnrate
  FROM    (
          select distinct user_id,
                          session_id,
-                         session_number
+                         session_number, device_id
 
     FROM views
 WHERE project_id = {project_id}
@@ -72,16 +102,17 @@ AND device_id in ({device_id})
 
      on next_session_info.user_id = user_id_filter.user_id and
      toUInt64(next_session_info.session_number) = toUInt64(user_id_filter.session_number) + 1
-
+ GROUP BY device_id
     SETTINGS distributed_group_by_no_merge = 0;
   """
 
 
 query_daydiffebetweentwosessions = """ 
-SELECT uniqExact(users),
+SELECT device_id, uniqExact(users),
        if(nbrdaydifferencbetweentwosessions >10, +10,nbrdaydifferencbetweentwosessions ) as newnbrdaydifferencbetweentwosessions
 
-FROM (SELECT user_id_filter.session_date                                                                firstdate,
+FROM (SELECT user_id_filter.device_id as device_id,
+user_id_filter.session_date                                                                firstdate,
              next_session_info.session_date                                                          as returndate,
              dateDiff('day', firstdate, returndate) as nbrdaydifferencbetweentwosessions,
              user_id_filter.user_id as users
@@ -94,7 +125,7 @@ FROM (SELECT user_id_filter.session_date                                        
                                session_number,
                                session_time,
                                session_date,
-                               path
+                               path, device_id
 
                FROM views
           WHERE project_id = {project_id}
@@ -124,9 +155,9 @@ AND device_id in ({device_id})
               toUInt64(next_session_info.session_number) = toUInt64(user_id_filter.session_number) + 1
 
            --  WHERE dateDiff('minute', user_id_filter.session_time, next_session_info.session_time) <= 180000
-      GROUP BY user_id_filter.session_date, next_session_info.session_date, users, next_session_info.session_time, user_id_filter.session_time )
+      GROUP BY user_id_filter.session_date, next_session_info.session_date, users, next_session_info.session_time,device_id, user_id_filter.session_time )
 
-GROUP BY newnbrdaydifferencbetweentwosessions
+GROUP BY newnbrdaydifferencbetweentwosessions, device_id
 HAVING newnbrdaydifferencbetweentwosessions >= 0
 ORDER BY newnbrdaydifferencbetweentwosessions asc
     SETTINGS distributed_group_by_no_merge = 0;
@@ -196,6 +227,129 @@ query_boucereturners = """
 
 
   SELECT user_id_filter.device_id, uniqExact(user_id_filter.session_id)                                                    as firstsessioninfo,
+                   uniqExactIf(user_id_filter.session_id, user_id_filter.is_last ) as bouncersfirstsession,
+                   bouncersfirstsession/ firstsessioninfo as bounceratefirstsession,
+                   uniqExactIf(next_session_info.session_id, user_id_filter.is_last) as bouncerreturners, --(number of sessions who have bounced during their first sessions and came back on the website n+1)
+                   bouncerreturners /bouncersfirstsession as bouncersreturnersrate -- (among the sessions who have bounced, % who came back on the website),
+
+
+
+      -- Put info about the first session
+      FROM (
+
+               select distinct user_id,
+                               session_id,
+                               session_number,
+                               session_time,
+                               session_date,
+                               view_number, is_last, device_id, session_number_of_transactions
+
+
+
+               FROM views
+               WHERE  is_first
+        AND  project_id = {project_id}
+        AND session_date >= '{start_date}'
+        AND session_date <= '{end_date}'
+        AND device_id in ({device_id})
+      
+               ) as user_id_filter
+
+               LEFT JOIN
+
+
+           (select user_id,
+                   device_id,
+                   session_id,
+                   session_number,
+                   session_date,
+                   session_time,
+                   path,
+                   session_number_of_transactions
+
+
+
+            FROM views
+        WHERE  project_id = {project_id}
+        AND session_date >= '{start_date}'
+        AND session_date <= '{end_date}'
+        AND device_id in ({device_id})
+               ) as next_session_info
+           on next_session_info.user_id = user_id_filter.user_id and
+              toUInt64(next_session_info.session_number) = toUInt64(user_id_filter.session_number) + 1
+              GROUP BY user_id_filter.device_id
+             SETTINGS distributed_group_by_no_merge = 0;
+  """
+
+
+# Page level
+
+query_nbrofsessionspervisitorpagelevel = """ 
+SELECT device_id, nbrofsessions, uniqExact(user_id) as nbrofusers
+    FROM
+    (
+   WITH ({page_condition}) as Page
+SELECT  uniqExact(session_number) as sum_session, user_id ,
+       if(sum_session >5, 5, sum_session) as nbrofsessions, device_id
+
+
+
+FROM views
+WHERE project_id = {project_id}
+AND session_date >= '{start_date}'
+AND session_date <= '{end_date}'
+AND device_id in ({device_id})
+AND Page
+GROUP BY user_id, device_id
+    )
+GROUP BY nbrofsessions, device_id
+ORDER BY nbrofsessions asc
+SETTINGS distributed_group_by_no_merge = 0;
+  """
+
+query_returonthesiteafterexitingonapage = """ 
+SELECT
+
+    device_id, uniqExact(user_id_filter.session_id)          as exit_firstsession,
+    uniqExact(next_session_info.session_id)       as return_secondsession,
+    return_secondsession/ exit_firstsession as sessionsreturnrate
+ FROM    (  WITH (({page_condition})) as Page
+         select distinct user_id,
+                         session_id,
+                         session_number, device_id
+
+    FROM views
+WHERE project_id = {project_id}
+AND session_date >= '{start_date}'
+AND session_date <= '{end_date}'
+AND device_id in ({device_id})
+AND Page
+AND is_last = 1
+
+         ) as user_id_filter
+
+   LEFT JOIN
+
+(select user_id,
+             session_id,
+             session_number
+
+
+    FROM views
+  WHERE project_id = {project_id}
+AND session_date >= '{start_date}'
+AND session_date <= '{end_date}'
+AND device_id in ({device_id})
+
+         ) as next_session_info
+
+     on next_session_info.user_id = user_id_filter.user_id and
+     toUInt64(next_session_info.session_number) = toUInt64(user_id_filter.session_number) + 1
+ GROUP BY device_id
+    SETTINGS distributed_group_by_no_merge = 0;
+  """
+
+query_boucerspagelevelcomingbackonthesite = """   SELECT user_id_filter.device_id, uniqExact(user_id_filter.session_id)                                                    as firstsessioninfo,
                    uniqExactIf(user_id_filter.session_id, user_id_filter.is_last ) as bouncersfirstsession,
                    bouncersfirstsession/ firstsessioninfo as bounceratefirstsession,
                    uniqExactIf(next_session_info.session_id, user_id_filter.is_last) as bouncerreturners, --(number of sessions who have bounced during their first sessions and came back on the website n+1)
